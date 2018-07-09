b32548db2ab754a0ade633ddcef4b30fd8ff95dd
felhix <felix.gaude@gmail.com>
2018-07-09 14:04:15 +0200

    timer.seconds viré

--- 05_timer/timer_spec.rb
+++ 05_timer/timer_spec.rb
@@ -7,17 +7,14 @@ describe 'time_string' do
   end
 
   it "should display 12 seconds as 00:00:12" do
-    @timer.seconds = 12
     expect(time_string(12)).to eq("00:00:12")
   end
 
   it "should display 66 seconds as 00:01:06" do
-    @timer.seconds = 66
     expect(time_string(66)).to eq("00:01:06")
   end
 
   it "should display 4000 seconds as 01:06:40" do
-    @timer.seconds = 4000
     expect(time_string(4000)).to eq("01:06:40")
   end
 end
